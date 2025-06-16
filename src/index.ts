b49e342852e402ef7cc76fc3a063c14c10070590
alwil17 <willialfred24@gmail.com>
2025-06-16 12:12:26 +0000

    refactor: add TODO comment to enhance issue opened event handler

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Probot } from "probot";
 
 export default (app: Probot) => {
+  // TODO: enhance this method
   // This will run when the issue is opened
   // and will create a comment on the issue
   app.on("issues.opened", async (context) => {
