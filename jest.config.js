c03876cefc25fab1398c71b3270d0417241b9af4
alwil17 <willialfred24@gmail.com>
2025-06-17 10:18:19 +0000

    feat: add initial Jest configuration for testing

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  roots: ['<rootDir>/tests/'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.[tj]sx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/index.ts'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
+};
