7dd51ebdc2585e6360097adb498a97d1716e3657
alwil17 <willialfred24@gmail.com>
2025-06-18 11:11:28 +0000

    refactor: remove unnecessary console log and clean up comments

--- function.js
+++ function.js
@@ -1,15 +1,14 @@
 import { createNodeMiddleware, Probot } from "probot";
 import appFn from "./dist/handlers/app.js";
 
-console.log("AutoMaintainer App Initialized");
+// Ensure environment variables are set
 const probot = new Probot({
   appId: process.env.APP_ID,
   privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'),
   secret: process.env.WEBHOOK_SECRET,
 });
 
-probot.load(appFn);
-
+// Register the app function with Probot
 export const probotApp = createNodeMiddleware(appFn, {
   probot: probot,
   webhooksPath: "/",
