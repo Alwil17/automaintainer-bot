027d6f9f8ffb041c89863e704c6d610b516d73e5
alwil17 <willialfred24@gmail.com>
2025-06-18 00:08:53 +0000

    refactor: restructure middleware setup and export probotApp function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
 import { createNodeMiddleware, createProbot } from "probot";
 import appHandler from "./handlers/app.js";
 
-export default createNodeMiddleware(appHandler, { probot: createProbot() });
+const middleware = createNodeMiddleware(appHandler, {
+  probot: createProbot(),
+  webhooksPath: "/",
+});
+
+import { IncomingMessage, ServerResponse } from "http";
+
+export const probotApp = (req: IncomingMessage, res: ServerResponse) => {
+  middleware(req, res, () => {
+    res.writeHead(404);
+    res.end();
+  });
+};
