21391f106435f703ca54167427cbb696fc5810f4
alwil17 <willialfred24@gmail.com>
2025-06-18 10:08:13 +0000

    fix: refactor probot initialization and middleware configuration

--- function.js
+++ function.js
@@ -1,7 +1,16 @@
-import { createNodeMiddleware, createProbot } from "probot";
+import { createNodeMiddleware, Probot } from "probot";
 import appFn from "./dist/handlers/app.js";
 
+console.log("AutoMaintainer App Initialized");
+const probot = new Probot({
+  appId: process.env.APP_ID,
+  privateKey: process.env.PRIVATE_KEY,
+  secret: process.env.WEBHOOK_SECRET,
+});
+
+probot.load(appFn);
+
 export const probotApp = createNodeMiddleware(appFn, {
-  probot: createProbot(),
+  probot: probot,
   webhooksPath: "/",
 });
