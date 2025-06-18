2ee03dcf31023f83ef5593732d037800e0aaaf02
alwil17 <willialfred24@gmail.com>
2025-06-18 04:09:57 +0000

    feat: add appHandler function to register event handlers for Probot app

---
+++ dist/handlers/app.d.ts
@@ -0,0 +1,7 @@
+import { Probot } from "probot";
+/**
+ * Registers all event handlers with the Probot app.
+ *
+ * @param {Probot} app - The Probot app to register the event handlers with.
+ */
+export default function appHandler(app: Probot): void;
---
+++ dist/handlers/app.js
@@ -0,0 +1,15 @@
+import handleIssuesOpened from "../events/issueOpened.js";
+import handlePush from "../events/push.js";
+import handlePullRequestOpened from "../events/pullRequestOpened.js";
+/**
+ * Registers all event handlers with the Probot app.
+ *
+ * @param {Probot} app - The Probot app to register the event handlers with.
+ */
+export default function appHandler(app) {
+    app.log.info("AutoMaintainer App Initialized");
+    app.on("issues.opened", handleIssuesOpened);
+    app.on("push", handlePush);
+    app.on("pull_request.opened", handlePullRequestOpened);
+}
+//# sourceMappingURL=app.js.map
