2440814072d6801672f780bd4b3cea57aa6633f0
alwil17 <willialfred24@gmail.com>
2025-06-18 00:08:28 +0000

    feat: add initial implementation of Probot webhook handler

---
+++ function.js
@@ -0,0 +1,24 @@
+// function.js
+const { Probot } = require("probot");
+const appFn = require("./lib/index"); // ton bot compilé
+const probot = new Probot({});
+
+probot.load(appFn);
+
+module.exports.webhook = (req, res) => {
+  probot.webhooks
+    .receive({
+      id: req.get("x-github-delivery"),
+      name: req.get("x-github-event"),
+      payload: req.body,
+    })
+    .then(() => res.status(200).send("OK"))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error");
+    });
+};
+
+const { probotApp } = require("./lib/index.js");
+
+exports.probotApp = probotApp;
