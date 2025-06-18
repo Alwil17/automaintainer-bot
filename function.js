2fafac4f3c6c1be0487265886422d1d598d2969d
alwil17 <willialfred24@gmail.com>
2025-06-18 02:27:38 +0000

    fix: refactor probot app initialization to use createNodeMiddleware

--- function.js
+++ function.js
@@ -1,24 +1,4 @@
-// function.js
-const { Probot } = require("probot");
-const appFn = require("./lib/index");
-const probot = new Probot({});
+const { createNodeMiddleware, createProbot } = require("probot");
+const app = require("./lib/handlers/app");
 
-probot.load(appFn);
-
-module.exports.webhook = (req, res) => {
-  probot.webhooks
-    .receive({
-      id: req.get("x-github-delivery"),
-      name: req.get("x-github-event"),
-      payload: req.body,
-    })
-    .then(() => res.status(200).send("OK"))
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error");
-    });
-};
-
-const { probotApp } = require("./lib/index.js");
-
-exports.probotApp = probotApp;
+exports.probotApp = createNodeMiddleware(app, { probot: createProbot(), webhooksPath: "/" });
